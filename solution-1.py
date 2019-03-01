3851f7361767a510d39440a076904e3c41031e15
karolina belzowska <belzowskakarolina@gmail.com>
2019-03-01 12:02:42 +0000

    Created solution 1

---
+++ solution-1.py
@@ -0,0 +1,7 @@
+# solution to problem 1
+# Karolina Szafran Belzowska, 2019-02-24
+# The sum of all numbers between 1 and 10
+
+input("Enter a positive integer to get the sum of all numbers between 1 and 10:")
+
+print(int("1") + int("2") + int("3") + int("4") + int("5") + int("6") + int("7") + int("8") + int("9") + int("10"))
