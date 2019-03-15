f14191758634fbdee13f27259e30d38ddf18b099
karolina belzowska <belzowskakarolina@gmail.com>
2019-03-15 09:50:42 +0000

    Created solution 2

---
+++ solution-2.py
@@ -0,0 +1,9 @@
+# Karolina Szafran Belzowska, 2019-03-15
+# Is it a day that begins with letter T?
+
+import datetime
+
+if datetime.datetime.today().weekday() == 1 and 3:
+    print("Yes,today is a day which begins with a letter T")
+else:
+    print("No, today is a day which does not begins with a letter T")
