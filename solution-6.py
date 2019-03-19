51552a503f23767272ab592bdb328136964b021b
karolina belzowska <belzowskakarolina@gmail.com>
2019-03-19 19:16:40 +0000

    Created solution 6

---
+++ solution-6.py
@@ -0,0 +1,14 @@
+# Karolina Szafran-Belzowska, 2019-03-19
+# secondstring #
+
+words = "The quick brown fox jumps over the lazy dog."
+
+
+splitted = words.split()
+
+first = splitted[1]
+second = splitted[3]
+third = splitted[5]
+fourth = splitted[7]
+
+print(first,second,third,fourth)
