d0ab5090b80166048c9e1c179983bbdc8e3220ca
karolina belzowska <belzowskakarolina@gmail.com>
2019-03-27 20:46:27 +0000

    Created solution 7

---
+++ solution-7.py
@@ -0,0 +1,14 @@
+# solution to problem number 7
+# Karolina Szafran Belzowska, 2019/03/27
+# An approximation of a square root ( Newton's method)
+
+# The number to calculate the square root of 14.5
+rootof = 14.5 
+# The initial estimate for the square root
+estimate = 1.0
+
+
+while abs((estimate * estimate) - rootof) > 0.1:
+    estimate -= (( estimate * estimate) - rootof) / (2 * estimate)
+
+print(f"The square root of {rootof} is approx. {estimate}.")
