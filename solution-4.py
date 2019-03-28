ac3d6a1504cadff321514742732b425164b97254
Karolina  Szafran - Belzowska <47725412+karolinaszafranbelzowska@users.noreply.github.com>
2019-03-28 22:48:17 +0000

    Update solution-4.py

--- solution-4.py
+++ solution-4.py
@@ -1,5 +1,6 @@
 # Karolina Szafran-Belzowska, 2019-03-16
 # collatz.py
+# Adapted from https://stackoverflow.com/questions/33508034/making-a-collatz-program-automate-the-boring-stuff?answertab=active#tab-top
 
 def collatzSequence(number):
     if (number % 2 == 0): # if it's even
