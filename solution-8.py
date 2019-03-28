dbb42b6331a2631638ee0db357d84f840b712bef
Karolina  Szafran - Belzowska <47725412+karolinaszafranbelzowska@users.noreply.github.com>
2019-03-28 23:04:10 +0000

    Update solution-8.py

--- solution-8.py
+++ solution-8.py
@@ -1,5 +1,6 @@
 # Karolina Szafran Belzowska, 2018-03-18
 # Today is?
+# This video helped me to solve the problem https://www.youtube.com/watch?v=HDe8InbWQJU,
 
 import datetime
 today = datetime.datetime.today()
