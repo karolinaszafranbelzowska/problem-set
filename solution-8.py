e7ea57539f5fb144328fe632caeae453570fc9ac
Karolina  Szafran - Belzowska <47725412+karolinaszafranbelzowska@users.noreply.github.com>
2019-03-28 22:17:25 +0000

    Update solution-8.py

--- solution-8.py
+++ solution-8.py
@@ -6,11 +6,4 @@ today = datetime.datetime.today()
 f = "%A, %B %dth, %Y at %H:%M"
 s = today.strftime(f)
 
-if (3 <= day <= 21) or (23 <= day <= 31):
-
-    s = str(s) + 'th'
-else:
-    suffixes = {1: 'st', 2: 'nd', 3: 'rd'}
-    s = str(s) + suffixes[s % 10]
-    
 print("Today is:", s)
